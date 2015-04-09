5de3a57c1b85488edfce71346cb5e5cd9ba639f3
jzorrof@gmail.com <jzorrof@gmail.com>
2015-04-09 11:48:45 +0800

    modify for heroku

--- myflaskr.py
+++ myflaskr.py
@@ -9,16 +9,24 @@ from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash, _app_ctx_stack
 
-#configuration
-DATABASE = '/tmp/flaskr.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
+# DATABASE = '/tmp/flaskr.db'
+# DEBUG = True
+# SECRET_KEY = 'development key'
+# USERNAME = 'admin'
+# PASSWORD = 'default'
 
 #crete my app
 app = Flask(__name__)
-app.config.from_object(__name__)
+
+#configuration
+#load setting as dict
+app.config.update(dict(
+    DATABASE = os.path.join(app.root_path, 'flaskr.db'),
+    DEBUG = True,
+    SECRET_KEY = 'development key',
+    USERNAME = 'admin',
+    PASSWORD = 'default'
+))
 app.config.from_envvar('MYFLASKR_SETTINGS', silent = True)
 # config end
 
@@ -35,31 +43,44 @@ create table entries (
 """
 #init _db from schema.sql
 def init_db():
-    with app.app_context():
-        db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
+    """init database"""
+    db = get_db()
+    with app.open_resource('schema.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+
+@app.cli.command('init_db')
+def initdb_command():
+    init_db()
+    print('Initialized the database.')
 
 # open a new db connection
 def get_db():
-    top = _app_ctx_stack.top
-    if not hasattr(top, 'sqlite_db'):
-        sqlite_db = sqlite3.connect(app.config['DATABASE'])
-        sqlite_db.row_factory = sqlite3.Row
-        top.sqlite_db = sqlite_db
-    return top.sqlite_db
+    # top = _app_ctx_stack.top
+    # if not hasattr(top, 'sqlite_db'):
+    #     sqlite_db = sqlite3.connect(app.config['DATABASE'])
+    #     sqlite_db.row_factory = sqlite3.Row
+    #     top.sqlite_db = sqlite_db
+    # return top.sqlite_db
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
 
 def connect_db():
     rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.ROW
+    rv.row_factory = sqlite3.Row
     return rv
 
+# @app.teardown_appcontext
+# def close_db_connection(exception):
+#     top = _app_ctx_stack.top
+#     if hasattr(top, 'sqlite_db'):
+#         top.sqlite_db.close()
 @app.teardown_appcontext
-def close_db_connection(exception):
-    top = _app_ctx_stack.top
-    if hasattr(top, 'sqlite_db'):
-        top.sqlite_db.close()
+def close_db(error):
+    """Close the database"""
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
 
 @app.route('/')
 def show_entries():
@@ -97,8 +118,4 @@ def login():
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
-    return redirect(url_for('show_entries')) 
-
-if __name__ == '__main__':
-    init_db()
-    app.run()
+    return redirect(url_for('show_entries'))
